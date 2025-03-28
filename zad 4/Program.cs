613629a6ff6df94a9715376e532fca0873e13dc5
MEGA <MEGA@gmail.com>
2025-03-28 15:17:06 +0200

    zad 4 is fully finished!

--- zad 4/Program.cs
+++ zad 4/Program.cs
@@ -9,8 +9,11 @@
             Console.WriteLine("Write a target sum:");
             int target = int.Parse(Console.ReadLine());
             int[] result = TwoSum(array, target);
-
             Console.WriteLine($"The result is: index: {string.Join(' ', result)}, numbers: {array[result[0]]} {array[result[1]]}");
+
+            Console.WriteLine("Without dictionary, it is:");
+            int[] result2 = WithoutDictionary(array, target);
+            Console.WriteLine($"The result is: index: {string.Join(' ', result2)}, numbers: {array[result2[0]]} {array[result2[1]]}");
         }
 
         static public int[] TwoSum(int[] nums, int target)
@@ -21,9 +24,23 @@
                 int complement = target - nums[i];
                 if (dictionary.ContainsKey(complement) && dictionary[complement] != i)
                 {
-                    return new int[] { i, dictionary[complement] };
+                    return new int[] { dictionary[complement], i };
                 }
                 dictionary.Add(nums[i], i);
+            }
+            return null;
+        }
+        static public int[] WithoutDictionary(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
             }
             return null;
         }
